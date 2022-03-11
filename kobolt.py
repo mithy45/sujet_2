88e92792fffd15591c732b459a10dd24d04eb2a7
quentin1.guyot <quentin1.guyot@epita.fr>
2022-03-11 11:45:34 +0100

    Kobolt

--- kobolt.py
+++ kobolt.py
@@ -5,6 +5,9 @@ class Kobolt(Character):
     def __init__(self, name, ad = 50, max_hp = 50):
         super().__init__(name, "Kobolt", max_hp, ad, max_hp)
 
+    def __init__(self):
+        super().__init__("Vilain", "Kobolt", 30, 5, 30)
+
     def get_damage(self, ad):
         self.current_hp -= int(ad * 0.75)
         if self.current_hp < 0:
