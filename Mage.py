8782071eaedc07f6a89024b89089d2e1fa95596a
quentin1.guyot <quentin1.guyot@epita.fr>
2022-03-11 11:16:05 +0100

    Mage fini

--- Mage.py
+++ Mage.py
@@ -1,4 +1,6 @@
+from Character import Character
+
 
 class Mage(Character):
-    def __init__(self):
-        super().__init()
+    def __init__(self, name, ad=80, max_hp=50):
+        super().__init__(name, "Mage", max_hp, ad, max_hp)
